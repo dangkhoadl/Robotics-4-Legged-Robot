fdb3944dc0f7b01f8185bc454b889d5bfc0fcfb9
dangkhoa1992 <dangledangkhoa@gmail.com>
2017-09-25 13:55:05 +0800

    Book Keeping

--- OUT/filter.d
+++
@@ -1,4 +0,0 @@
-.\out\filter.o: lib_user\filter.c
-.\out\filter.o: lib_user\filter.h
-.\out\filter.o: lib_user\matrix.h
-.\out\filter.o: E:\Program Files\Keil\ARM\ARMCC\bin\..\include\stdlib.h
--- OUT/matrix.d
+++
@@ -1,4 +0,0 @@
-.\out\matrix.o: lib_user\matrix.c
-.\out\matrix.o: lib_user\matrix.h
-.\out\matrix.o: E:\Program Files\Keil\ARM\ARMCC\bin\..\include\stdlib.h
-.\out\matrix.o: E:\Program Files\Keil\ARM\ARMCC\bin\..\include\math.h
